a1ecefecad6a00355a8f88e62b94015befbd2201
handson1 <238203@unai.edu>
2024-11-17 21:38:43 +0700

    handson 8

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import config.Database;
+import entities.repositories.TodoListRepositoryDbImpl;
+import entities.repositories.TodoListRepositoryImpl;
 import repositories.TodoListRepository;
-import repositories.TodoListRepositoryImpl;
-import services.TodoListService;
-import services.TodoListServiceImpl;
-import views.TodoListTerminalView;
-import views.TodoListView;
+import entities.repositories.TodoListRepositoryImpl;
+import entities.services.TodoListService;
+import entities.service.TodoListServiceImpl;
+import entities.views.TodoListTerminalView;
+import entities.views.TodoListView; // Fixed typo
+
 
 public class Main {
     public static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        Database database = new Database("db_michael", "root", "", "Localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new TodoListRepositoryDbImpl(database);
+
+
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
-        TodoListView todoListView = new TodoListTerminalView(todoListService);
+
+
+        TodoListView todoListView = (TodoListView) new TodoListTerminalView(todoListService);
+
         todoListView.run();
     }
 }
